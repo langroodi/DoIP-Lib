c859da68c145a99071a34e07b236722583ffbc92
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2022-09-01 20:05:25 +0200

    :beer: Add GenericNack class: - Corresponding unit tests are added to the project.

---
+++ include/doiplib/generic_nack.h
@@ -0,0 +1,34 @@
+#ifndef GENERIC_NACK_H
+#define GENERIC_NACK_H
+
+#include "./message.h"
+#include "./nack_type.h"
+
+namespace DoipLib
+{
+    class GenericNack : public Message
+    {
+    private:
+        static const PayloadType cPayloadType{
+            PayloadType::GenericNegativeAcknowledgement};
+
+        NackType mNackCode;
+
+    protected:
+         void GetPayload(std::vector<uint8_t> &payload) override;
+
+         virtual bool TrySetPayload(const std::vector<uint8_t> &payload) override;
+         
+    public:
+        /// @brief Constructor
+        /// @param protocolVersion DoIP ISO protocol version
+        /// @param nackCode Negative acknowledgement code
+        GenericNack(uint16_t protocolVersion, NackType nackCode) noexcept;
+
+        /// @brief Get NACK code
+        /// @return Negative acknoweledgement code
+        NackType GetNackCode() const noexcept;
+    };
+}
+
+#endif
---
+++ src/generic_nack.cpp
@@ -0,0 +1,41 @@
+#include "doiplib/generic_nack.h"
+
+namespace DoipLib
+{
+    const PayloadType GenericNack::cPayloadType;
+
+    GenericNack::GenericNack(
+        uint16_t protocolVersion,
+        NackType nackCode) noexcept : Message(protocolVersion, cPayloadType),
+                                      mNackCode{nackCode}
+    {
+    }
+
+    void GenericNack::GetPayload(std::vector<uint8_t> &payload)
+    {
+        auto _nackCodeByte{static_cast<uint8_t>(mNackCode)};
+        payload.insert(payload.begin(), _nackCodeByte);
+    }
+
+    bool GenericNack::TrySetPayload(const std::vector<uint8_t> &payload)
+    {
+        const std::size_t cNackCodeIndex{cHeaderSize};
+
+        if (payload.size() > cNackCodeIndex)
+        {
+            uint8_t _nackCodeByte{payload.at(cNackCodeIndex)};
+            mNackCode = static_cast<NackType>(_nackCodeByte);
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    NackType GenericNack::GetNackCode() const noexcept
+    {
+        return mNackCode;
+    }
+}
