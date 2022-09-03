5f54e7bdd3eb7abc86251b07ed55700a1d1b2180
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2022-09-03 14:00:37 +0200

    :beer: Implement VinVehicleIdRequest class: - Corresponding unit tests are added to the project.

---
+++ src/vin_vehicle_id_request.cpp
@@ -0,0 +1,47 @@
+#include <stdexcept>
+#include "doiplib/vin_vehicle_id_request.h"
+
+namespace DoipLib
+{
+    const PayloadType VinVehicleIdRequest::cPayloadType;
+    const std::size_t VinVehicleIdRequest::cVinSize;
+
+    VinVehicleIdRequest::VinVehicleIdRequest() noexcept : Message(cPayloadType)
+    {
+    }
+
+    VinVehicleIdRequest::VinVehicleIdRequest(
+        uint16_t protocolVersion, std::string vin) : Message(protocolVersion, cPayloadType),
+                                                     mVin{vin.size() == cVinSize ? vin : throw std::out_of_range("Invalid vehicle ID number")}
+    {
+    }
+
+    void VinVehicleIdRequest::GetPayload(std::vector<uint8_t> &payload)
+    {
+        payload = std::vector<uint8_t>(mVin.cbegin(), mVin.cend());
+    }
+
+    bool VinVehicleIdRequest::TrySetPayload(const std::vector<uint8_t> &payload)
+    {
+        const std::size_t cExpectedSize{
+            static_cast<std::size_t>(cHeaderSize + cVinSize)};
+
+        if (payload.size() == cExpectedSize)
+        {
+            auto _beginItr{payload.cbegin() + cHeaderSize};
+
+            mVin = std::string(_beginItr, payload.end());
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    std::string VinVehicleIdRequest::GetVin() const
+    {
+        return mVin;
+    }
+}
