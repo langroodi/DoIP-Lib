bbb6d7caebc44b184f4a0d85e65fedddeab5c278
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2022-09-09 21:09:26 +0200

    :syringe: Fix InvalidDeserialization test bug

--- test/vehicle_id_response_test.cpp
+++ test/vehicle_id_response_test.cpp
@@ -1,6 +1,5 @@
 #include <iterator>
 #include <algorithm>
-#include <utility>
 #include <gtest/gtest.h>
 #include <doiplib/vehicle_id_response.h>
 
@@ -175,7 +174,7 @@ namespace DoipLib
     TEST(VehicleIdResponeTest, InvalidDeserialization)
     {
         const std::vector<uint8_t> cSerializedMessage{
-            0x02, 0xfd, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00};
+            0x02, 0xfd, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00};
 
         VehicleIdResponse _message;
         bool _succeed{_message.TryDeserialize(cSerializedMessage)};
